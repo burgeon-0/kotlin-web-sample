281adaae9a2d3c66d6cbf47a763cb1bd575bb15c
Sam Lu <luxiaocong524@163.com>
2023-02-10 15:17:50 +0800

    [feat] 修改样例代码

---
+++ web-support/src/main/kotlin/org/bg181/kotlin/common/FieldCopyExtension.kt
@@ -0,0 +1,132 @@
+package org.bg181.kotlin.common
+
+import org.springframework.beans.BeanUtils
+import org.springframework.beans.BeanWrapper
+import org.springframework.beans.BeanWrapperImpl
+import org.springframework.cglib.beans.BeanCopier
+import java.beans.PropertyDescriptor
+
+/**
+ * <p>
+ * 提供属性复制（field copy）函数，可以为我们带来便利，如下面的场景：
+ * 1. 不使用属性复制函数时：
+ * <pre>
+ *     val notebookBo = NotebookBo().apply {
+ *         uid = notebookPo.uid
+ *         title = notebookPo.title
+ *         description = notebookPo.description
+ *         ......
+ *     }
+ * </pre>
+ * 2. 使用属性复制函数时：
+ * <pre>
+ *     val notebookBo = notebookPo.convert(NotebookBo::class.java)
+ * </pre>
+ * </p>
+ *
+ * <p>
+ * 提供属性复制（field copy）函数，面临着两个问题，一是安全性问题，二是性能问题。
+ * 1. 安全性问题：对于目标对象的不可变[val]属性，属性复制会失效。为解决此问题，当目标对象存在不可变[val]属性时，函数将抛出异常。
+ * （其核心问题在于，对于不可变[val]属性，复制不起作用，如果开发人员不清楚，将带来巨大的安全隐患。）
+ * 2. 性能问题：[FieldCopyExtensionBenchmarkTest]
+ * 如有其他看法，欢迎补充。
+ * </p>
+ */
+class FieldCopyExtension
+
+/**
+ * 【扩展】对象转换函数
+ *
+ * @param clazz 指定需要转换成哪个类的对象
+ * @param ignoreProperties 排除的属性
+ * @return 转换后的对象
+ */
+fun <T : Any> Any.convert(clazz: Class<T>, vararg ignoreProperties: String): T {
+    return this.convert(clazz, true, *ignoreProperties)
+}
+
+/**
+ * 【扩展】对象转换函数
+ *
+ * @param clazz 指定需要转换成哪个类的对象
+ * @param ignoreNullValue 排除空值
+ * @param ignoreProperties 排除的属性
+ * @return 转换后的对象
+ */
+fun <T : Any> Any.convert(clazz: Class<T>, ignoreNullValue: Boolean, vararg ignoreProperties: String): T {
+    val obj = clazz.newInstance()
+    checkIfExistImmutableField(obj)
+    var ignores = when (ignoreNullValue) {
+        true -> getNullPropertyNames(this)
+        else -> listOf<String>()
+    }
+    if (ignoreProperties.isNotEmpty()) {
+        ignores = ignores.plus(ignoreProperties)
+    }
+    BeanUtils.copyProperties(this, obj, *ignores.toTypedArray())
+    return obj
+}
+
+/**
+ * 【扩展】对象填充函数
+ *
+ * @param source 数据来源对象
+ * @param ignoreProperties 排除的属性
+ * @return 对象本身
+ */
+fun <T : Any> T.fillBy(source: Any, vararg ignoreProperties: String): T {
+    return this.fillBy(source, true, *ignoreProperties)
+}
+
+/**
+ * 【扩展】对象填充函数
+ *
+ * @param source 数据来源对象
+ * @param ignoreNullValue 排除空值
+ * @param ignoreProperties 排除的属性
+ * @return 对象本身
+ */
+fun <T : Any> T.fillBy(source: Any, ignoreNullValue: Boolean, vararg ignoreProperties: String): T {
+    checkIfExistImmutableField(this)
+    var ignores = when (ignoreNullValue) {
+        true -> getNullPropertyNames(source)
+        else -> listOf<String>()
+    }
+    if (ignoreProperties.isNotEmpty()) {
+        ignores = ignores.plus(ignoreProperties)
+    }
+    BeanUtils.copyProperties(source, this, *ignores.toTypedArray())
+    return this
+}
+
+/**
+ * 检查是否存在不可变属性
+ *
+ * @param target
+ */
+private fun checkIfExistImmutableField(target: Any) {
+    val className = target::class.java.name
+    val tar: BeanWrapper = BeanWrapperImpl(target)
+    val pds: Array<PropertyDescriptor> = tar.getPropertyDescriptors()
+    pds.filter {
+        it.name != "class" && tar.getPropertyDescriptor(it.name).writeMethod == null
+    }.map {
+        throw IllegalAccessException("${className} - 不可变属性(val ${it.name})的值不可变更!!!")
+    }
+}
+
+/**
+ * 获取值为空的属性
+ *
+ * @param source
+ * @return
+ */
+private fun getNullPropertyNames(source: Any): List<String> {
+    val src: BeanWrapper = BeanWrapperImpl(source)
+    val pds: Array<PropertyDescriptor> = src.getPropertyDescriptors()
+    return pds.filter {
+        src.getPropertyValue(it.name) == null
+    }.map {
+        it.name
+    }
+}
---
+++ web-support/src/test/kotlin/org/bg181/kotlin/common/FieldCopyExtensionTest.kt
@@ -0,0 +1,141 @@
+package org.bg181.kotlin.common
+
+import org.bg181.kotlin.common.convert
+import org.bg181.kotlin.common.fillBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.slf4j.LoggerFactory
+import java.io.Serializable
+
+/**
+ * 扩展函数影响范围较广，如果修改了 FieldCopyExtension，一定要跑一下单元测试，测试通过才可以发布代码
+ *
+ * @author lxc
+ * @date 2023/02/10
+ */
+class FieldCopyExtensionTest {
+
+    private val logger = LoggerFactory.getLogger(FieldCopyExtensionTest::class.java)
+
+    @Test
+    fun test() {
+        logger.info("====== normal test ======")
+        normalTest()
+        logger.info("====== null value test ======")
+        nullValueTest()
+        logger.info("====== override null value test ======")
+        overrideNullValueTest()
+    }
+
+    /**
+     * 常规功能测试
+     */
+    private fun normalTest() {
+        // 测试 fillBy 函数，和 ignoreProperties 参数
+        val person = Person("sam", 3)
+        val student = Student("lily", 12)
+        // Person to Student
+        student.fillBy(person, "name")
+        logger.info("person: $person")
+        assert(person.name == "sam" && person.age == 3)
+        logger.info("student: $student")
+        assert(student.name == "lily" && student.age == 3)
+        student.fillBy(person)
+        logger.info("student: $student")
+        assert(student.name == "sam" && student.age == 3)
+        // Student to Student
+        student.fillBy(Student("lucy", 21))
+        logger.info("student: $student")
+        assert(student.name == "lucy" && student.age == 21)
+
+        // 测试 convert 函数，和 ignoreProperties 参数
+        // Person to Student
+        val student2 = person.convert(Student::class.java)
+        logger.info("student2: $student2")
+        assert(student2.name == "sam" && student2.age == 3)
+        val student3 = person.convert(Student::class.java, "name")
+        logger.info("student3: $student3")
+        assert(student3.name == null && student3.age == 3)
+        // Student to Student
+        val student4 = student.convert(Student::class.java)
+        logger.info("student4: $student4")
+        assert(student4.name == "lucy" && student4.age == 21)
+        val student5 = student.convert(Student::class.java, "age")
+        logger.info("student5: $student5")
+        assert(student5.name == "lucy" && student5.age == null)
+
+        // Teacher 含有 val 属性，会抛出异常
+        // TODO 疑问：为何这里抛出的异常行数与真实代码的行数对不上，assert 抛出异常时却可以对上的？
+        assertThrows<Exception> {
+            val teacher = person.convert(Teacher::class.java)
+            logger.info("teacher: $teacher")
+        }
+        // Teacher 含有 val 属性，会抛出异常
+        assertThrows<Exception> {
+            val teacher2 = Teacher()
+            logger.info("teacher2: $teacher2")
+            teacher2.fillBy(person)
+            logger.info("teacher2: $teacher2")
+        }
+    }
+
+    /**
+     * 空值测试
+     */
+    private fun nullValueTest() {
+        // 测试 fillBy 函数，和 ignoreProperties 参数
+        val person = Person(null, 3)
+        val student = Student("lily", 12)
+        // Person to Student
+        student.fillBy(person, "age")
+        logger.info("person: $person")
+        assert(person.name == null && person.age == 3)
+        logger.info("student: $student")
+        assert(student.name == "lily" && student.age == 12)
+        student.fillBy(person)
+        logger.info("student: $student")
+        assert(student.name == "lily" && student.age == 3)
+        // Student to Student
+        student.fillBy(Student("lucy", null))
+        logger.info("student: $student")
+        assert(student.name == "lucy" && student.age == 3)
+    }
+
+    /**
+     * 空值覆盖测试
+     */
+    private fun overrideNullValueTest() {
+        // 测试 fillBy 函数，和 ignoreProperties 参数
+        val person = Person(null, 3)
+        val student = Student("lily", 12)
+        // Person to Student
+        student.fillBy(person, false, "age")
+        logger.info("person: $person")
+        assert(person.name == null && person.age == 3)
+        logger.info("student: $student")
+        assert(student.name == null && student.age == 12)
+        student.fillBy(person, false)
+        logger.info("student: $student")
+        assert(student.name == null && student.age == 3)
+        // Student to Student
+        student.fillBy(Student("lucy", null), false)
+        logger.info("student: $student")
+        assert(student.name == "lucy" && student.age == null)
+    }
+
+    /**
+     * TODO 疑问：为何这里的 data class 一定要有无参构造函数，study-sample-server 的 data class 没有无参构造函数也可以跑成功？
+     */
+    data class Person(var name: String?, var age: Int?) : Serializable {
+        constructor() : this(null, null)
+    }
+
+    data class Student(var name: String?, var age: Int?) : Serializable {
+        constructor() : this(null, null)
+    }
+
+    data class Teacher(val name: String?, val age: Int?) : Serializable {
+        constructor() : this(null, null)
+    }
+
+}
